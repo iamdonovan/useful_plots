dc9e18df0dbb6307552b35ee1052eb8e01a01c62
Luc Girod <luc.girod@gmail.com>
2022-06-21 10:29:26 +0200

    Pushed the future a tiny bit, and separated Sentinel 2A/B

--- satellite_mission_diagram.py
+++ satellite_mission_diagram.py
@@ -31,7 +31,7 @@ def annotate_patch(ax, patch, label):
 
     box_width = width_ / width * (len(label) * 12 + 15) # figure out how big the text will be (12 pt font, 15 pt buffer)
 
-    if width < len(label) * 12:  # if our bar is too small, text goes to the left
+    if width < len(label) * 10:  # if our bar is too small, text goes to the left
         start = start - box_width
         if start < ax.get_xlim()[0]: # if the box is too far left, text goes on the right
             start = patch.get_bbox().xmax
@@ -49,9 +49,9 @@ def annotate_patch(ax, patch, label):
     return lab
 
 
-fig, ax = plt.subplots(1, 1, figsize=(24, 16))
+fig, ax = plt.subplots(1, 1, figsize=(26, 16))
 
-ax.set_xlim(1968, 2024)
+ax.set_xlim(1968, 2027)
 ax.tick_params(axis='x', labelsize=24)
 ax.set_yticks([])
 
@@ -62,7 +62,7 @@ ax.spines['top'].set_visible(False)
 res_labels = ['< 1 m', '1-10 m', '10-30 m', '30-100 m', '> 100 m']
 res_colors = ['blueviolet', 'cornflowerblue', 'lightseagreen', 'sandybrown', 'lightcoral']
 
-today = 2022.25 # current year-ish
+today = 2022.5 # current year-ish
 
 # lists of the satellite data.
 # form is (name, start, end, position on graph, res_colors index, alpha)
@@ -75,21 +75,23 @@ landsat = [('Landsat 1 MSS', 1972.5, 1978, 3, 1),
            ('Landsat 6 ETM+', 1993.7, 1993.8, 2, 1),
            ('Landsat 7 ETM+', 1999.25, 2003.5, 2, 1),
            ('SLC-Off', 2003.5, today, 2, 0.5),
-           (None, today, 2025, 2, 0.2),
+           (None, today, 2028, 2, 0.2),
            ('Landsat 8 OLI/TIRS', 2013, today, 2, 1),
-           (None, today, 2025, 2, 0.2),
+           (None, today, 2028, 2, 0.2),
            ('Landsat 9 OLI/TIRS', 2021.75, today, 2, 1),
-           (None, today, 2025, 2, 0.2)]
+           (None, today, 2028, 2, 0.2)]
 
 # terra
 terra = [('ASTER', 1999.9, today, 2, 1),
-         (None, today, 2025, 2, 0.2),
+         (None, today, 2028, 2, 0.2),
          ('MODIS', 1999.9, today, 4, 1),
-         (None, today, 2025, 4, 0.2)]
+         (None, today, 2028, 4, 0.2)]
 
 # sentinel-2
-sentinel = [('Sentinel-2 MSI', 2015.5, today, 2, 1),
-            (None, today, 2025, 2, 0.2)]
+sentinel = [('Sentinel-2A MSI', 2015.5, today, 2, 1),
+            (None, today, 2028, 2, 0.2),
+            ('Sentinel-2B MSI', 2017.2, today, 2, 1),
+            (None, today, 2028, 2, 0.2)]
 
 # SPOT
 spot = [('SPOT 1', 1986, 1991, 2, 1),
@@ -98,41 +100,41 @@ spot = [('SPOT 1', 1986, 1991, 2, 1),
         ('SPOT 4', 1998.25, 2013.5, 2, 1),
         ('SPOT 5', 2002.25, 2015.25, 1, 1),
         ('SPOT 6', 2012.75, today, 1, 1),
-        (None, today, 2025, 1, 0.2),
+        (None, today, 2028, 1, 0.2),
         ('SPOT 7', 2014.5, today, 1, 1),
-        (None, today, 2025, 1, 0.2)]
+        (None, today, 2028, 1, 0.2)]
 
 # ALOS-PRISM
 alos = [('ALOS PRISM', 2006, 2011.5, 1, 1),
         ('ALOS-2 PRISM', 2014.5, today, 1, 1),
-        (None, today, 2025, 1, 0.2)]
+        (None, today, 2028, 1, 0.2)]
 
 # pléiades
 pleiades = [('Pléiades', 2012, today, 0, 1),
-            (None, today, 2025, 0, 0.2),
+            (None, today, 2028, 0, 0.2),
 			('Pléiades NEO', 2021, today, 0, 1),
-            (None, today, 2025, 0, 0.2)]
+            (None, today, 2028, 0, 0.2)]
 
 # DigitalGlobe/WorldView
 wv = [('IKONOS', 1999.75, 2015.25, 0, 1),
       ('QuickBird', 2001.75, 2015, 0, 1),
       ('GeoEye-1', 2008.75, today, 0, 1),
-      (None, today, 2025, 0, 0.2),
+      (None, today, 2028, 0, 0.2),
       ('WorldView-1', 2007.75, today, 0, 1),
-      (None, today, 2025, 0, 0.2),
+      (None, today, 2028, 0, 0.2),
       ('WorldView-2', 2009.75, today, 0, 1),
-      (None, today, 2025, 0, 0.2),
+      (None, today, 2028, 0, 0.2),
       ('WorldView-3', 2014.5, today, 0, 1),
-      (None, today, 2025, 0, 0.2),
+      (None, today, 2028, 0, 0.2),
       ('WorldView-4', 2016.9, 2019, 0, 1),
 #      ('WorldView Legion', 2022.5, today, 0, 1), #Not spaceborn yet
-#      (None, today, 2025, 0, 0.2)
+#      (None, today, 2028, 0, 0.2)
     ]
 
 rapideye = [('RapidEye', 2008.75, 2020.25, 1, 1)]
 
 planet = [('Planet', 2010.5, today, 1, 1),
-          (None, today, 2025, 1, 0.2)]
+          (None, today, 2028, 1, 0.2)]
 
 # plot and annotate all of the sensors - change combination as you see fit.
 this_pos = 0
@@ -156,7 +158,7 @@ for sens in landsat + terra + sentinel + spot + alos + pleiades + wv + rapideye 
 ax.set_ylim(0, this_pos)
 
 # plot vertical ticks every 5 years...
-for yr in range(1970, 2025, 5):
+for yr in range(1970, 2028, 5):
     ax.plot([yr, yr], [0, this_pos], 'k--', linewidth=0.5)
 
 # plot the present-day
