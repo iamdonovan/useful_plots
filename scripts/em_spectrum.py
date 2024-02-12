68dddb304c86eb68c37e059e422d212eeeea2d04
Bob McNabb <robertmcnabb@gmail.com>
2024-02-12 20:26:50 +0000

    add final versio of EM spectrum diagram

--- scripts/em_spectrum.py
+++ scripts/em_spectrum.py
@@ -27,7 +27,7 @@ def rainbow_patch(x, dx, h, c):
     return plt.Rectangle((x, -1), dx, h, color=c)
 
 
-fig = plt.figure(figsize=(15, 5))
+fig = plt.figure(figsize=(15, 3))
 ax0 = fig.add_subplot(111)
 ax0.set_frame_on(False)
 # ax.spines['right'].set_visible(False)
@@ -35,10 +35,18 @@ ax0.set_frame_on(False)
 ax0.set_xticks([])
 ax0.set_yticks([])
 
-gs = gridspec.GridSpec(10, 7, hspace=0.1)
+# ax0.vlines([0.3772, 0.3892], 0.68, 0.8, colors='k', linestyles='dashed', lw=1)
+ax0.plot([0.2929, 0.3772], [0.326, 0.665], 'k--', lw=1)
+ax0.plot([0.3892, 0.5604], [0.665, 0.326], 'k--', lw=1)
 
-ax = fig.add_subplot(gs[0:2, :])
-vis_ax = fig.add_subplot(gs[5, 2:4])
+ax0.set_xlim(0, 1)
+ax0.set_ylim(0, 1)
+
+
+gs = gridspec.GridSpec(9, 7, hspace=0.1)
+
+ax = fig.add_subplot(gs[1:3, :])
+vis_ax = fig.add_subplot(gs[6, 2:4])
 
 # ax.set_xticks([])
 ax.set_yticks([])
@@ -59,17 +67,23 @@ for name in regions:
     ax.add_patch(p)
     annotate_patch(ax, p, rdict[name]['full'])
 
-ax.semilogx()
-ax.set_xlim(lower[0], upper[-1])
+# ax.semilogx()
 ax.set_ylim(-1, 1)
+ax.set_xscale('log')
+ax.set_xlim(lower[0], upper[-1])
 
-ax.set_xticks([10**x for x in range(-16, 10, 2)])
-# ax.axis('equal')
+freqs = list(range(24, -1, -2))
+wavelengths = lambda2nu(np.array([10**x for x in range(24, -1, -2)], dtype=float))
+
+ax.set_xticks([float(10**x) for x in range(-16, 10, 2)])
+
+ax2 = ax.secondary_xaxis('top')
+ax2.set_xticks(wavelengths, labels=['$10^{' + str(f) + '}$' for f in freqs])
 
 vis = np.linspace(380, 720, 100) / 1e9
 dx = vis[1] - vis[0]
 
-vis_cmap = plt.get_cmap('gist_rainbow_r')
+vis_cmap = plt.get_cmap('rainbow')
 
 for n, x in enumerate(vis):
     color = vis_cmap(n / vis.size)
@@ -84,8 +98,10 @@ vis_ax.set_yticks([])
 
 vis_ax.set_xlabel('wavelength (nm)')
 
-ax.annotate(r'$\lambda$ (m)', (1.0, -0.05), xycoords='axes fraction', ha='right', va='top')
+ax.annotate(r'$\lambda$ (m)', (1.0, -0.03), xycoords='axes fraction', ha='right', va='top')
 ax.annotate(r'$\nu$ (Hz)', (1.0, 1.05), xycoords='axes fraction', ha='right', va='bottom')
 
-ax0.annotate(r'Increasing wavelength ($\lambda$) $\rightarrow$', (0.6, 0.65), xycoords='axes fraction')
-ax0.annotate(r'$\leftarrow$ Increasing frequency ($\nu$)', (0.6, 1.10), xycoords='axes fraction')
+ax0.annotate(r'Increasing wavelength ($\lambda$) $\rightarrow$', (0.6, 0.48), xycoords='axes fraction')
+ax0.annotate(r'$\leftarrow$ Increasing frequency ($\nu$)', (0.6, 1.05), xycoords='axes fraction')
+
+fig.savefig('figures/ElectromagneticSpectrum.png', bbox_inches='tight')
